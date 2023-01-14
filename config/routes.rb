287e82fc26da90cf88cf17ef4ebcff612b9999b1
hoonkim <hoonk9387@gmail.com>
2023-01-14 13:16:25 -0600

    Updated route update route with patch /birds/:id.

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   get "/birds", to: "birds#index"
   get "/birds/:id", to: "birds#show"
   post "/birds", to: "birds#create"
+  patch "/birds/:id", to: "birds#update"
 end
