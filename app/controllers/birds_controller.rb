a733210c288be4bfc35fcf124a81108079461e1a
hoonkim <hoonk9387@gmail.com>
2023-01-14 13:12:48 -0600

    Updated route with post /birds birds#create. Added create method for post route. Added protect_from_forgery with: :null_session to ApplicationController.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -5,10 +5,15 @@ class BirdsController < ApplicationController
         birds = Bird.all
         render json: birds
     end
-
     # GET /birds/:id
     def show
         bird = Bird.find_by(id: params[:id])
         render json: bird
     end
+    # POST /birds
+    def create
+        bird = Bird.create(name: params[:name], species: params[:species])
+        render json: bird, status: :created
+    end
+
 end
