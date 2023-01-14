48fd365b56f693ece3fd8addce3558e542405976
hoonkim <hoonk9387@gmail.com>
2023-01-13 19:38:27 -0600

    Added routes to /birds and /birds/:id.

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # GET    /birds(.:format) birds#index
-  # 
   # POST   /birds(.:format) birds#create
-  # 
   # GET    /birds/new(.:format) birds#new
-  # 
   # GET    /birds/:id/edit(.:format) birds#edit
   # GET    /birds/:id(.:format) birds#show
   # PATCH  /birds/:id(.:format) birds#update
   # PUT    /birds/:id(.:format) birds#update
   # DELETE /birds/:id(.:format) birds#destroy
 
-  resources :birds
+  # resources :birds
+
+  get "/birds", to: "birds#index"
+  get "/birds/:id", to: "birds#show"
 end
